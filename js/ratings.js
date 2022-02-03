f83dd33e6ad0c73f6e1b24afe93fef76eac41c78
Marchewka <filip.marchewka@capgemini.com>
2022-02-03 15:46:27 +0100

    done

--- js/ratings.js
+++ js/ratings.js
@@ -1,6 +1,3 @@
-
-
-
 function collect_ratings(){
     const ratings = {
         count: 0,
@@ -9,13 +6,25 @@ function collect_ratings(){
     }
     let rating = 0;
 
-    const elements = document.querySelectorAll(".ratings")
+    const elements = document.querySelectorAll(".rating")
     console.log(elements)
 
     elements.forEach((element)=>{
-        rating = parseInt(element.id.replace("star", " "))
+        rating = parseInt(element.id.replace("star", " "));
 
         ratings.count += parseInt(element.value)
+        ratings.sum += parseInt(element.value) * rating;
     })
+
+    if(ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+
+    return ratings
 }
+
+document.addEventListener("change", () => {
+    const ratings = collect_ratings();
+    document.querySelector('#average').value = ratings.average.toFixed(2);
+})
 
